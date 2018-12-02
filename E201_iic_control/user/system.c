36d1636042749dff5a539f3a637b3860870f8915
ysd <ysd@emomo.com>
2018-12-02 18:02:30 +0800

    [master]IIC没有调试成功

--- E201_iic_control/user/system.c
+++ E201_iic_control/user/system.c
@@ -1,16 +1,36 @@
 #include "system.h"
 
+#define APP_SET_BACKLIGHT(X)		( App_Para.Tp_LedSta &= (unsigned char)(X))
+#define APP_CLR_BACKLIGHT(X)		( App_Para.Tp_LedSta |= (unsigned char)(~X))
+
+
 unsigned char Uart0SendFlag,Uart0ReceiveFlag;
 
 unsigned char Rev_Buf;
 
 TIMEOUT_PARA TimeOut_Para[2];
 
+static xdata App_Para_t App_Para;
+static xdata IIC_PARA_t I2c_Para[IIC_NUM];
+
+
 void System_Init(void)
 {
+   int i;
    TimeOutDet_Init();
    Led_Init(); 
-   LED1_SET(1);    
+   LED1_SET(1);
+   for(i = 0;i < IIC_NUM;i ++){
+		I2c_Para[i].ID = i;
+		if(i == 0){
+			I2c_Para[i].read_addr = I2C_READ_ADDR;
+			I2c_Para[i].write_addr = I2C_WRITE_ADDR;
+		}
+		IIC_Init(&I2c_Para[i]);
+	}
+	P1PH |= 0X80; // i2c int 
+   APP_CLR_BACKLIGHT(0x40);	//0x40    0xbf
+   IIC_Write_Data(&I2c_Para[0], I2c_Para[0].write_addr, App_Para.Tp_LedSta);
    Timer_Init();
    Uart_Init(); 
 }
@@ -30,6 +50,7 @@ unsigned char Rev_Byte(void)
 
 void System_Handle(void)
 {
+  #if 0
   static unsigned int Cnt; 
   if(0x77 == Rev_Byte())
   {
@@ -47,10 +68,83 @@ void System_Handle(void)
      }
      else 
      {
-        //;            
+        IIC_Write_Data(&I2c_Para[0], I2c_Para[0].write_addr, App_Para.Tp_LedSta);           
      }
   }
-  
+  #endif
+  	if(!Get_I2C_INT()){
+		IIC_Read_Data(&I2c_Para[0], I2c_Para[0].read_addr, &App_Para.TP_GetKey);
+	
+		switch(App_Para.TP_GetKey){
+			case 0x01:{
+				if(App_Para.Tp_CurKey != 0x01){
+					App_Para.Tp_CurKey = 0x01;
+					TimeOut_Record(&App_Para.LockLong_DetTimer[1], 100);
+					App_Para.LockDetFlag[1] = 1;
+				}
+				break;
+			}
+
+			case 0x02:{
+				if(App_Para.Tp_CurKey != 0x02){
+					App_Para.Tp_CurKey = 0x02;
+					//App_Key4Handle();//制冷/制热
+					//App_Key1Handle(); //制冷/制热
+				}
+				break;
+			}
+
+			case 0x04:{
+				if(App_Para.Tp_CurKey != 0x04){
+					App_Para.Tp_CurKey = 0x04;
+					//App_Key5Handle();   //脚灯
+				}
+				break;
+			}
+
+			case 0x08:{
+				if(App_Para.Tp_CurKey != 0x08){
+					App_Para.Tp_CurKey = 0x08;
+					//App_Key1Handle(); //制冷/制热
+					//App_Key4Handle();//制冷/制热
+				}
+				break;
+			}
+			
+			case 0x10:{
+				if(App_Para.Tp_CurKey != 0x10){
+					App_Para.Tp_CurKey = 0x10;
+					//App_Key3Handle();
+				}
+				break;
+			}
+
+			case 0x20:{
+				if(App_Para.Tp_CurKey != 0x20){
+					App_Para.Tp_CurKey = 0x20;
+					//App_Key2Handle();
+				}
+				break;
+			}
+			default:{
+				if(App_Para.Tp_CurKey){
+					App_Para.Tp_CurKey = 0;
+					//LED1_PIN = OFF; 
+					//LED2_PIN = OFF;
+					//APP_CLR_BACKLIGHT(TP_KEY5_LED);
+					//APP_CLR_BACKLIGHT(TP_KEY3_LED);
+					IIC_Write_Data(&I2c_Para[0], I2c_Para[0].write_addr, App_Para.Tp_LedSta);
+					App_Para.LockDetFlag[1] = 0;					
+					//if(App_Para.Pushrod_Status != PUSHROD_STOP){
+					//	App_Para.UpDataFlag = True;
+					//	App_Para.Pushrod_Status = PUSHROD_STOP;
+					//}
+				}
+				break;
+			}
+		}
+	}
+	
 }
 
 /**************************************************
@@ -97,3 +191,26 @@ void Bsp_Uart1IqrHandle() interrupt 7
         Uart0ReceiveFlag = 1;
     }
 }
+
+void I2c_Irq() interrupt 7{
+	/*<主机发送数据>*/
+	if( (SSCON0&0x07) == 0x02){
+		unsigned char get_data = SSDAT;
+		if(get_data != I2C_ADDR){
+			//App_LedHandle(get_data);
+		}
+	}
+	/*<主机接收数据>*/
+	else if( (SSCON0&0x07) == 0x03 ){
+		if(I2C_IntGet() == 0){
+			I2C_IntSet(1);
+		}
+	}
+	/*<必须手动清掉此位>*/	
+	SSCON0 &= ~0X40;
+}
+
+
+
+
+
--- E201_iic_control/user/system.h
+++ E201_iic_control/user/system.h
@@ -7,8 +7,44 @@
 #include "TimeOut.h"
 #include "bsp_timer.h"
 #include "bsp_uart.h"
+#include "bsp_i2c_s.h"
+#include "bsp_i2c.h"
+
+#define Get_I2C_INT()				P17
 
 
+#define I2C_READ_ADDR			0X5B
+#define I2C_WRITE_ADDR			0X5A
+
+
+typedef enum{
+	STOP_MODE = 0,
+	HEAT_MODE,
+	COOL_MODE
+}Heat_Cool_Sta_t;
+
+typedef struct{
+	unsigned char LockDetFlag[2],LockFlag;			/*<´¦Àí°´¼üËø¼ü>*/
+	Heat_Cool_Sta_t HC_Sta;
+	unsigned char TP_GetKey,Chg_GetKey;			
+	unsigned char Tp_LedSta,Tp_LedStaBak;			/*<¼ÇÂ¼LEDµÆ×´Ì¬>*/
+	unsigned char Tp_CurKey,Chg_CurKey;	
+	TIMEOUT_PARA LockLong_DetTimer[2];
+	unsigned int SentData;
+	unsigned char FootLedSta;
+    unsigned char MASSAGESta;
+    unsigned char WarmSta;
+	unsigned char Pushrod_Status;
+	unsigned char UpDataFlag;
+	unsigned char ShutDownFlag;
+	unsigned char OnLineCnt,OnLineFlag;
+    unsigned char MassAutoClsFlag,WarmAutoClsFlag;
+	unsigned char CoolAutoClsFlag,HeatAutoClsFlag,SystemAutoClsFlag,
+TpLedDisCntFlag;
+	unsigned int CoolAutoCnt,HeatAutoCnt,SystemAutoCnt,TpLedDisCnt;
+    unsigned int MassAutoCnt,WarmAutoCnt;
+	
+}App_Para_t;
 
 void System_Init(void);
 void System_Handle(void);
